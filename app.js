b35362207a0eaff75f50206169607f808ad7f1ae
Andrea <anlramirezs11@gmail.com>
2019-11-13 15:10:16 -0500

    Se genera prueba.js

--- app.js
+++ app.js
@@ -62,3 +62,13 @@ var enviarMision = function (xmen) {
 };
 enviarMision(wolverine);
 //Clases
+var Avenger = /** @class */ (function () {
+    function Avenger(nombre, equipo, nombreReal) {
+        this.nombre = nombre;
+        this.equipo = equipo;
+        this.nombreReal = nombreReal;
+    }
+    return Avenger;
+}());
+var antman = new Avenger("Antman", "Cap", "Scott Lang");
+//Constructores
